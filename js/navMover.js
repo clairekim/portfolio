900e2a9f60eabeb925d37917e781e9cd7692a52e
hyeeunkim <llcomicll@gmail.com>
2017-05-23 16:57:23 +0900

    메서드 분리

--- js/navMover.js
+++ js/navMover.js
@@ -1,37 +1,43 @@
-(function($){ 
-
+(function($){
 	function NavMover(selector, options){
 		//프로퍼티 생성
 		this.$nav = null;
 		this.$section = null;
 
 		this.init(selector, options);
-		//this.navMove();
-		//this.scrollMove();
+		this.navMove();
+		this.scrollMove();
 	}
-	//요소초기화 
+	//요소초기화 : this. 붙여주기 ->  NavMover의 속성임을 지정
 	NavMover.prototype.init = function(selector, options){
 		this.$nav =  $(selector).children();
 		this.$section = $( options.sectionName );		
-		var objThis = this; 
+		this.$options = options; 
+	}
 
+	//메서드 : this의 scope에 유의
+	NavMover.prototype.navMove = function(){	
+		var objThis = this; //메서드 안에서의 this는 메서드를 가진 객체. cf) 메서드 내부 중첩함수에서 this -> window
 		//네비 클릭
+		this.$nav.eq(0).addClass('on');
 		this.$nav.on('click focusin', 'a', function() { 
 			$(this).parent().addClass('on').siblings().removeClass('on');
 			var i = $(this).parent().index();
-			var secTopPos = objThis.$section.eq(i).offset().top - options.topMargin;
-			$("html, body").stop().animate({scrollTop:secTopPos}, options.duration, options.easing);
+			var secTopPos = objThis.$section.eq(i).offset().top - objThis.$options.topMargin;
+			$("html, body").stop().animate({scrollTop:secTopPos}, objThis.$options.duration, objThis.$options.easing);
 			return false;
 		});
 
+	 }
+	NavMover.prototype.scrollMove = function(){ 
+		var objThis = this;
 		//윈도우 스크롤 이동시
 		$(window).scroll(function() {
 			var scT = $(window).scrollTop();
 			var winHalf = $(window).outerHeight() / 2;
 			var pos = 0;
-			//console.log(objThis.$section)
 			objThis.$section.each(function(i){
-				pos = $(this).offset().top - options.topMargin - winHalf ;
+				pos = $(this).offset().top - objThis.$options.topMargin - winHalf;
 				if(scT >= pos){
 					objThis.$nav.eq(i).addClass('on').siblings().removeClass('on');	
 				}
@@ -39,16 +45,13 @@
 		});
 	}
 
-	//메서드
-	// NavMover.prototype.navMove = function(){ }
-	// NavMover.prototype.scrollMove = function(){ }
-
 	$.defaultOptions = {
 		duration : 500,
 		easing: "easeInOutCubic",
 		sectionName : "section",
 		topMargin : 0
 	}
+
 	$.fn.navMover = function(options){
         options = $.extend(null, $.defaultOptions, options);
 		this.each(function(index){ 
