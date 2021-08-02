e620fdd379dca9a5c16fed0bd9c59262123558f1
clairekim@gmail.com <llcomicll@gmail.com>
2021-08-03 03:25:39 +0900

    2021 포트폴리오 코드정리

--- js/common.js
+++ js/common.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {	
 	
-	// 제작 플러그인 적용, 섹션이동
+	// 제작 플러그인 초기화, 섹션이동
 	$(".gnb").navMover({
 		duration : 700,
 		sectionName: '.content > section', //class, id, element
 		topMargin : 80
 	});
 
-	//포트폴리오 스와이퍼
+	//스와이퍼 초기화
 	var portSwiper = new Swiper('.port .swiper-container', {
 		slidesPerView: 1,
 		slidesPerGroup:1,
@@ -32,11 +32,11 @@ $(document).ready(function() {
 		}
 	  });
 
-	//quick/로고 상단이동
-	$(".quick-top a, .logo a").on('click', function(e) {
+	//quick/로고 이동
+	var $quickTop = $(".quick-top");
+	$quickTop.on("click", "a", function(e) {
 		e.preventDefault();
 		$("html, body").animate({scrollTop:0}, 500, "easeInOutCubic");
-		return false;
 	});
 	
 	//quick 나타나기/감추기
@@ -44,20 +44,23 @@ $(document).ready(function() {
 		var locate = $(this).scrollTop();
 		var wHeight = $(window).height();
 		if (locate < wHeight) {
-			$(".quick-top").css("visibility","hidden");
+			$quickTop.css("visibility","hidden");
 		} else if (locate >= wHeight){
-			$(".quick-top").css("visibility","visible");
+			$quickTop.css("visibility","visible");
 		}
 	});	
 
-	//히스토리, 스크롤 애니메이션 
+	//스크롤 반응 애니메이션 
 	$(window).on('scroll', function(){
 		var screenQuarter = ($(window).height()/4)*3;
 		var scrollPos = $(window).scrollTop() + screenQuarter;
-		$('.list-history li').each(function(index){
+		var $lstHistory = $('.list-history');
+		$lstHistory.find('li').each(function(){
 			var liPos = $(this).offset().top;			
 			if( !$(this).hasClass('open') && scrollPos >= liPos) {
 				$(this).addClass('open');
+			} else if ( $(this).hasClass('open') && scrollPos < liPos ) {
+				$(this).removeClass('open');
 			}
 		});
 	});	
